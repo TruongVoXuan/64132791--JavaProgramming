58fd7df8edb0d117dba594dca017237ac0e029b8
TruongVoXuan <truong.75.cvan@gmail.com>
2024-11-15 13:41:30 +0700

    Update HelloApplication.java

--- Shape/src/main/java/truongvx/shape/HelloApplication.java
+++ Shape/src/main/java/truongvx/shape/HelloApplication.java
@@ -22,13 +22,13 @@ public class HelloApplication extends Application {
         gc.strokePolygon(new double[]{200, 50, 350}, new double[]{50, 250, 250}, 3);
 
         // Làm đoạn cung 1 dài ra từ trái qua
-        gc.strokeArc(125, 210, 150, 150, 0, 50, ArcType.OPEN);   // Đoạn cung 1 (dài ra)
-        gc.strokeArc(125, 225, 150, 150, 120, 80, ArcType.OPEN); // Đoạn cung 2
-        gc.strokeArc(125, 175, 150, 150, 240, 80, ArcType.OPEN); // Đoạn cung 3
-
+           // Đoạn cung 1 (dài ra)
+        gc.strokeArc(125, 225, 155, 150, -130, 80, ArcType.OPEN); // Đoạn cung 2
+        gc.strokeArc(125, 225, 150, 150, -38, 80, ArcType.OPEN); // Đoạn cung 3
+        gc.strokeArc(125, 225, 155, 150, -222, 80, ArcType.OPEN);
         // Vẽ hai hình tròn nhỏ bên dưới hình tròn lớn
-        gc.strokeOval(140, 300, 30, 30);
-        gc.strokeOval(230, 300, 30, 30);
+        gc.strokeOval(125, 345, 30, 30);
+        gc.strokeOval(250, 345, 30, 30);
 
         root.getChildren().add(canvas);
         primaryStage.setScene(new Scene(root));
